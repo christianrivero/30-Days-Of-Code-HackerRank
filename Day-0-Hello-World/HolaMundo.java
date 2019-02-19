2bb8d7db5b13d250fd1728964cfcd690034b58c1
Christian Rivero Valencia <42509107+christianrivero@users.noreply.github.com>
2019-02-19 09:51:08 -0500

    Add files via upload

---
+++ Day-0-Hello-World/HolaMundo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class HolaMundo
+{
+   public static void main(String[] args)
+   {
+      Scanner sc = new Scanner(System.in);
+
+      String inputString;
+
+      inputString = sc.nextLine();
+
+      System.out.println("Hello, World.");
+      System.out.println(inputString);
+   }
+}
